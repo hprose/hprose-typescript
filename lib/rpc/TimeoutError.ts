9f85ae98e1fd3b9384ab6eb1f13359b10551555c
andot <andot@hprose.com>
2018-12-26 14:07:34 +0800

    Added TimeoutError.ts

---
+++ lib/rpc/TimeoutError.ts
@@ -0,0 +1,22 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\*________________________________________________________*/
+/*--------------------------------------------------------*\
+|                                                          |
+| hprose/rpc/TimeoutError.ts                               |
+|                                                          |
+| TimeoutError for TypeScript.                             |
+|                                                          |
+| LastModified: Dec 26, 2018                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+export default class TimeoutError extends Error {
+    constructor(message: string) { super(message); }
+}
