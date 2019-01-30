4e242f2dc474be6e56a71503ff353da172a1e98b
andot <andot@hprose.com>
2019-01-30 22:30:25 +0800

    Improved rpc-plugin-log.

--- packages/@hprose/rpc-plugin-log/src/index.ts
+++ packages/@hprose/rpc-plugin-log/src/index.ts
@@ -19,26 +19,20 @@ import { Context, NextIOHandler, NextInvokeHandler } from '@hprose/rpc-core';
 export class Log {
     public static async ioHandler(request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> {
         console.log(ByteStream.toString(request));
-        try {
-            const response = await next(request, context);
-            console.log(ByteStream.toString(response));
-            return response;
-        }
-        catch(e) {
-            console.error(e);
-            throw e;
-        }
+        const response = next(request, context);
+        response.then(
+            (value) => console.log(ByteStream.toString(value)),
+            (reason) => console.error(reason)
+        );
+        return response;
     }
     public static async invokeHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
         const a = JSON.stringify(args);
-        try {
-            const result = await next(name, args, context);
-            console.log(`${name}(${a.substring(1, a.length - 1)}) = ${JSON.stringify(result)}`);
-            return result;
-        }
-        catch(e) {
-            console.error(e);
-            throw e;
-        }
+        const result = next(name, args, context);
+        result.then(
+            (value) => console.log(`${name}(${a.substring(1, a.length - 1)}) = ${JSON.stringify(value)}`),
+            (reason) => console.error(reason)
+        );
+        return result;
     }
 }
