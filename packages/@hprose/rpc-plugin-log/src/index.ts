a43befb8287820b611d11593807d5134366c6458
andot <andot@hprose.com>
2019-01-30 22:24:05 +0800

    Added error log.

--- packages/@hprose/rpc-plugin-log/src/index.ts
+++ packages/@hprose/rpc-plugin-log/src/index.ts
@@ -19,14 +19,26 @@ import { Context, NextIOHandler, NextInvokeHandler } from '@hprose/rpc-core';
 export class Log {
     public static async ioHandler(request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> {
         console.log(ByteStream.toString(request));
-        const response = await next(request, context);
-        console.log(ByteStream.toString(response));
-        return response;
+        try {
+            const response = await next(request, context);
+            console.log(ByteStream.toString(response));
+            return response;
+        }
+        catch(e) {
+            console.error(e);
+            throw e;
+        }
     }
     public static async invokeHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
         const a = JSON.stringify(args);
-        const result = await next(name, args, context);
-        console.log(`${name}(${a.substring(1, a.length - 1)}) = ${JSON.stringify(result)}`);
-        return result;
+        try {
+            const result = await next(name, args, context);
+            console.log(`${name}(${a.substring(1, a.length - 1)}) = ${JSON.stringify(result)}`);
+            return result;
+        }
+        catch(e) {
+            console.error(e);
+            throw e;
+        }
     }
 }
