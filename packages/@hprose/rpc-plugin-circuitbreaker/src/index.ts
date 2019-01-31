872e7b6d5eb719531813504a026f4240a87c233d
andot <andot@hprose.com>
2019-01-31 15:50:36 +0800

    Added  @hprose/rpc-plugin-circuitbreaker

---
+++ packages/@hprose/rpc-plugin-circuitbreaker/src/index.ts
@@ -0,0 +1,67 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| index.ts                                                 |
+|                                                          |
+| @hprose/rpc-plugin-circuitbreaker for TypeScript.        |
+|                                                          |
+| LastModified: Jan 31, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+import { Context, NextIOHandler, NextInvokeHandler } from '@hprose/rpc-core';
+
+export interface MockService {
+    invoke(name: string, args: any[], context: Context): Promise<any>;
+}
+
+export class BreakerError extends Error {
+    constructor(message: string = 'service breaked') { super(message); }
+}
+
+export class CircuitBreaker {
+    private lastFailTime: number = 0;
+    private failCount: number = 0;
+    public constructor(
+        public readonly threshold: number = 5,
+        public readonly recoverTime: number = 30000,
+        public readonly mock?: MockService) {
+    }
+    public async ioHandler(request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> {
+        if (this.failCount > this.threshold) {
+            const interval = Date.now() - this.lastFailTime;
+            if (interval < this.recoverTime) {
+                throw new BreakerError();
+            } else {
+                this.failCount = this.threshold >> 1;
+            }
+        }
+        const response = next(request, context);
+        response.then(() => {
+            if (this.failCount > 0) this.failCount = 0;
+        }, () => {
+            this.failCount++;
+            this.lastFailTime = Date.now();
+        });
+        return response;
+    }
+    public async invokeHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
+        if (this.mock) {
+            const mock = this.mock;
+            return next(name, args, context).catch((e) => {
+                if (e instanceof BreakerError) {
+                    return mock.invoke(name, args, context);
+                } else {
+                    throw e;
+                }
+            });
+        }
+        else {
+            return next(name, args, context);
+        }
+    }
+}
