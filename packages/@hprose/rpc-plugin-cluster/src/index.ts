9095790434040b9ede7728ad9af4fa80d3c7fffe
andot <andot@hprose.com>
2019-01-31 13:02:29 +0800

    Improved rpc-plugin-cluster

--- packages/@hprose/rpc-plugin-cluster/src/index.ts
+++ packages/@hprose/rpc-plugin-cluster/src/index.ts
@@ -8,7 +8,7 @@
 |                                                          |
 | @hprose/rpc-plugin-cluster for TypeScript.               |
 |                                                          |
-| LastModified: Jan 23, 2019                               |
+| LastModified: Jan 31, 2019                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -48,8 +48,8 @@ export class FailoverSettings implements ClusterSettings {
     }
 }
 
-export class FailbackSettings implements ClusterSettings {
-    static readonly default: ClusterSettings = new FailbackSettings();
+export class FailtrySettings implements ClusterSettings {
+    static readonly default: ClusterSettings = new FailtrySettings();
     public onretry: (context: Context) => number;
     constructor(public retry: number = 10, minInterval: number = 500, maxInterval: number = 5000) {
         this.onretry = (context: Context) => {
@@ -67,81 +67,83 @@ export class FailfastSettings implements ClusterSettings {
     constructor(public onfailure: (context: Context) => void) { }
 }
 
-export function getClusterHandler(settings: ClusterSettings = FailoverSettings.default): IOHandler {
-    if (settings.retry === undefined) settings.retry = 10;
-    if (settings.idempotent === undefined) settings.idempotent = false;
-    async function handler(request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> {
-        try {
-            const result = await next(request, context);
-            if (settings.onsuccess) {
-                settings.onsuccess(context);
-            }
-            return result;
-        }
-        catch (e) {
-            if (settings.onfailure) {
-                settings.onfailure(context);
-            }
-            if (settings.onretry) {
-                const idempotent = context.idempotent === undefined ? settings.idempotent : context.idempotent;
-                const retry = context.retry === undefined ? settings.retry : context.retry;
-                if (context.retried === undefined) {
-                    context.retried = 0;
+export class Cluster {
+    public static getHandler(settings: ClusterSettings = FailoverSettings.default): IOHandler {
+        if (settings.retry === undefined) settings.retry = 10;
+        if (settings.idempotent === undefined) settings.idempotent = false;
+        async function handler(request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> {
+            try {
+                const result = await next(request, context);
+                if (settings.onsuccess) {
+                    settings.onsuccess(context);
                 }
-                if (idempotent && context.retried < retry) {
-                    const interval = settings.onretry(context);
-                    if (interval > 0) {
-                        return new Promise<Uint8Array>((resolve, reject) => {
-                            setTimeout(() => {
-                                handler(request, context, next)
-                                    .then((result) => resolve(result))
-                                    .catch((reason) => reject(reason));
-                            }, interval);
-                        });
-                    } else {
-                        return handler(request, context, next);
+                return result;
+            }
+            catch (e) {
+                if (settings.onfailure) {
+                    settings.onfailure(context);
+                }
+                if (settings.onretry) {
+                    const idempotent = context.idempotent === undefined ? settings.idempotent : context.idempotent;
+                    const retry = context.retry === undefined ? settings.retry : context.retry;
+                    if (context.retried === undefined) {
+                        context.retried = 0;
+                    }
+                    if (idempotent && context.retried < retry) {
+                        const interval = settings.onretry(context);
+                        if (interval > 0) {
+                            return new Promise<Uint8Array>((resolve, reject) => {
+                                setTimeout(() => {
+                                    handler(request, context, next)
+                                        .then((result) => resolve(result))
+                                        .catch((reason) => reject(reason));
+                                }, interval);
+                            });
+                        } else {
+                            return handler(request, context, next);
+                        }
                     }
                 }
+                throw e;
             }
-            throw e;
         }
+        return handler;
     }
-    return handler;
-}
-
-export function forkingClusterHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
-    const clientContext = context as ClientContext;
-    const uris = clientContext.client.uris;
-    const n = uris.length;
-    const results: Promise<any>[] = new Array(n);
-    for (let i = 0; i < n; i++) {
-        const forkingContext = clientContext.clone();
-        forkingContext.uri = uris[i];
-        results[i] = next(name, args, forkingContext);
-    }
-    return new Promise<any>((resolve, reject) => {
-        const reasons: Error[] = new Array(n);
-        let count = n;
-        results.forEach((result, index) => {
-            result.then(resolve, (reason) => {
-                reasons[index] = reason;
-                if (--count === 0) {
-                    reject(reasons[0]);
-                }
+    
+    public static forkingHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
+        const clientContext = context as ClientContext;
+        const uris = clientContext.client.uris;
+        const n = uris.length;
+        const results: Promise<any>[] = new Array(n);
+        for (let i = 0; i < n; i++) {
+            const forkingContext = clientContext.clone();
+            forkingContext.uri = uris[i];
+            results[i] = next(name, args, forkingContext);
+        }
+        return new Promise<any>((resolve, reject) => {
+            const reasons: Error[] = new Array(n);
+            let count = n;
+            results.forEach((result, index) => {
+                result.then(resolve, (reason) => {
+                    reasons[index] = reason;
+                    if (--count === 0) {
+                        reject(reasons[0]);
+                    }
+                });
             });
         });
-    });
-}
-
-export function broadcastClusterHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
-    const clientContext = context as ClientContext;
-    const uris = clientContext.client.uris;
-    const n = uris.length;
-    const results: Promise<any>[] = new Array(n);
-    for (let i = 0; i < n; i++) {
-        const forkingContext = clientContext.clone();
-        forkingContext.uri = uris[i];
-        results[i] = next(name, args, forkingContext);
     }
-    return Promise.all(results);
+    
+    public static broadcastHandler(name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> {
+        const clientContext = context as ClientContext;
+        const uris = clientContext.client.uris;
+        const n = uris.length;
+        const results: Promise<any>[] = new Array(n);
+        for (let i = 0; i < n; i++) {
+            const forkingContext = clientContext.clone();
+            forkingContext.uri = uris[i];
+            results[i] = next(name, args, forkingContext);
+        }
+        return Promise.all(results);
+    }
 }
