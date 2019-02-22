12e8913b28ffa966fa98a6f02e7be1023d4c7ea2
小马哥 <mabingyao@gmail.com>
2019-02-22 16:59:05 +0800

    Update SocketHandler.ts

--- packages/@hprose/rpc-node/src/SocketHandler.ts
+++ packages/@hprose/rpc-node/src/SocketHandler.ts
@@ -8,7 +8,7 @@
 |                                                          |
 | SocketHandler for TypeScript.                            |
 |                                                          |
-| LastModified: Feb 12, 2019                               |
+| LastModified: Feb 22, 2019                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -35,7 +35,7 @@ export class SocketHandler {
     }
     private send(socket: net.Socket, response: Uint8Array, index: number): void {
         const n = response.length;
-        const header = new Buffer(12);
+        const header = Buffer.allocUnsafe(12);
         header.writeInt32BE(n | 0x80000000, 4);
         header.writeInt32BE(index, 8);
         const crc = crc32(header.subarray(4, 12));
