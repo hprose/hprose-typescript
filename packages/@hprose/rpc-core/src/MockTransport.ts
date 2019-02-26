59b0965dd4361b094f2368dd00fa99a38089fa72
小马哥 <mabingyao@gmail.com>
2019-02-27 03:19:25 +0800

    Create MockTransport.ts

---
+++ packages/@hprose/rpc-core/src/MockTransport.ts
@@ -0,0 +1,36 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| MockTransport.ts                                         |
+|                                                          |
+| MockTransport for TypeScript.                            |
+|                                                          |
+| LastModified: Feb 27, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+import { Context } from './Context';
+import { Client, Transport } from './Client';
+import { parseURI } from './Utils';
+import { MockManager } from './MockManager';
+
+export class MockTransport implements Transport {
+    public static readonly schemes: string[] = ['mock'];
+    async transport(request: Uint8Array, context: Context): Promise<Uint8Array> {
+        const uri = parseURI(context.uri);
+        return MockManager.handler(uri.hostname, request);
+    }
+    async abort(): Promise<void> { }
+}
+
+Client.register('mock', MockTransport);
+
+declare module '@hprose/rpc-core' {
+    export interface Client {
+        mock: MockTransport;
+    }
+}
