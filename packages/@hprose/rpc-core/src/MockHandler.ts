beae297378f9b5e4172d9f4d555677c21ee44d07
小马哥 <mabingyao@gmail.com>
2019-02-27 03:19:42 +0800

    Create MockHandler.ts

---
+++ packages/@hprose/rpc-core/src/MockHandler.ts
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| MockHandler.ts                                           |
+|                                                          |
+| MockHandler for TypeScript.                              |
+|                                                          |
+| LastModified: Feb 27, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+import { ServiceContext } from './ServiceContext';
+import { Service, Handler } from './Service';
+import { MockManager } from './MockManager';
+
+export interface MockServiceContext extends ServiceContext {
+    readonly handler: MockHandler;
+}
+
+export class MockServer {
+    public handler!: (address: string, request: Uint8Array) => Promise<Uint8Array>;
+    constructor(public readonly address: string) {}
+    public listen() {
+        MockManager.register(this.address, this.handler);
+    }
+    public close() {
+        MockManager.unregister(this.address);
+    }
+}
+
+export class MockHandler implements Handler {
+    constructor(public readonly service: Service) {}
+    public bind(server: MockServer): void {
+        server.handler = this.handler;
+    }
+    public handler = async (address: string, request: Uint8Array): Promise<Uint8Array> => {
+        if (request.length > this.service.maxRequestLength) {
+            throw new Error('Request entity too large');
+        }
+        const context = new ServiceContext(this.service);
+        context.family = 'mock';
+        context.address = address;
+        context.port = 0;
+        context.handler = this;
+        return this.service.handle(request, context);
+    }
+}
+
+Service.register('mock', MockHandler, [MockServer]);
+
+export interface Service {
+    mock: MockHandler;
+}
