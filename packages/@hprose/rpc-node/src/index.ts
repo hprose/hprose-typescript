ecede9a1f24d54edde95f63ac0b040bd737e48ba
小马哥 <mabingyao@gmail.com>
2019-02-27 03:21:22 +0800

    Update index.ts

--- packages/@hprose/rpc-node/src/index.ts
+++ packages/@hprose/rpc-node/src/index.ts
@@ -8,12 +8,12 @@
 |                                                          |
 | @hprose/rpc-node for TypeScript.                         |
 |                                                          |
-| LastModified: Jan 31, 2019                               |
+| LastModified: Feb 27, 2019                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
 
-export { HttpTransport,  HttpClientContext } from './HttpTransport';
+export { HttpTransport, HttpClientContext } from './HttpTransport';
 export { WebSocketTransport } from './WebSocketTransport';
 export { SocketTransport } from './SocketTransport';
 export { UdpTransport } from './UdpTransport';
