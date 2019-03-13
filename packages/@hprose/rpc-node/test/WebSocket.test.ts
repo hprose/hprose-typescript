c797baa2ec61f68e3e1956e7651b5cb50c7f0182
小马哥 <mabingyao@gmail.com>
2019-03-13 09:41:41 +0800

    Update WebSocket.test.ts

--- packages/@hprose/rpc-node/test/WebSocket.test.ts
+++ packages/@hprose/rpc-node/test/WebSocket.test.ts
@@ -1,7 +1,5 @@
-// import * as http from 'http';
 import WebSocket from 'ws';
-// import { Context, NextInvokeHandler, Service, Client, ClientContext, ServiceContext } from '@hprose/rpc-core';
-import { Service, Client } from '@hprose/rpc-core';
+import { Context, NextInvokeHandler, Service, Client, ClientContext, ServiceContext } from '@hprose/rpc-core';
 import '../src/index';
 
 test('test hello world rpc', async () => {
@@ -10,20 +8,17 @@ test('test hello world rpc', async () => {
     }
     const service = new Service();
     service.addFunction(hello);
-    //const server = http.createServer();
-    const wsserver = new WebSocket.Server({ port: 8087 });
-    service.bind(wsserver);
+    const server = new WebSocket.Server({ port: 8087 });
+    service.bind(server);
     service.websocket.compress = true;
-    //server.listen(8087);
     const client = new Client('ws://127.0.0.1:8087/');
     client.websocket.compress = true;
     const proxy = await client.useServiceAsync();
     const result = await proxy.hello('world');
     expect(result).toBe('hello world');
-    wsserver.close();
+    server.close();
 });
 
-/*
 test('test headers', async () => {
     const clientHandler = async (fullname: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> => {
         context.requestHeaders['ping'] = true;
@@ -43,10 +38,8 @@ test('test headers', async () => {
     const service = new Service();
     service.addFunction(hello);
     service.use(serviceHandler);
-    const server = http.createServer();
-    const wsserver = new WebSocket.Server({ server });
-    service.bind(wsserver);
-    server.listen(8088);
+    const server = new WebSocket.Server({ port: 8088 });
+    service.bind(server);
     const client = new Client('ws://127.0.0.1:8088/');
     client.use(clientHandler);
     const proxy = await client.useServiceAsync();
@@ -64,10 +57,8 @@ test('test maxRequestLength', async () => {
     const service = new Service();
     service.maxRequestLength = 10;
     service.addFunction(hello);
-    const server = http.createServer();
-    const wsserver = new WebSocket.Server({ server });
-    service.bind(wsserver);
-    server.listen(8089);
+    const server = new WebSocket.Server({ port: 8089 });
+    service.bind(server);
     const client = new Client('ws://127.0.0.1:8089/');
     const proxy = await client.useServiceAsync();
     try {
@@ -86,11 +77,9 @@ test('test ipaddress', async () => {
     }
     const service = new Service();
     service.add({ method: hello, fullname: 'hello', passContext: true });
-    const server = http.createServer();
-    const wsserver = new WebSocket.Server({ server });
-    service.bind(wsserver);
+    const server = new WebSocket.Server({ port: 8090 });
+    service.bind(server);
     service.websocket.compress = true;
-    server.listen(8090);
     const client = new Client('ws://127.0.0.1:8090/');
     client.websocket.compress = true;
     const proxy = await client.useServiceAsync();
@@ -102,4 +91,3 @@ test('test ipaddress', async () => {
     await proxy.hello('world 3');
     server.close();
 });
-*/
