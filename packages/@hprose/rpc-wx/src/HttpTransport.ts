e77c858b87bbce5783535b996b3c9c994707c498
andot <mabingyao@gmail.com>
2019-12-30 13:59:51 +0800

    Update HttpTransport.ts

--- packages/@hprose/rpc-wx/src/HttpTransport.ts
+++ packages/@hprose/rpc-wx/src/HttpTransport.ts
@@ -8,12 +8,12 @@
 |                                                          |
 | HttpTransport for TypeScript.                            |
 |                                                          |
-| LastModified: Dec 17, 2019                               |
+| LastModified: Dec 30, 2019                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
 
-import { Client, ClientContext, TimeoutError, Context, Transport } from '@hprose/rpc-core';
+import { Client, ClientContext, TimeoutError, Context, Transport, defer } from '@hprose/rpc-core';
 import { ByteStream } from '@hprose/io';
 
 export interface HttpClientContext extends ClientContext {
@@ -57,49 +57,48 @@ export class HttpTransport implements Transport {
         return headers;
     }
     public async transport(request: Uint8Array, context: Context): Promise<Uint8Array> {
-        const result = new Promise<Uint8Array>((resolve, reject) => {
-            const index = this.counter++;
-            let data: ArrayBuffer;
-            if (request.buffer.slice) {
-                data = request.buffer.slice(request.byteOffset, request.length);
-            } else {
-                const bytes = new Uint8Array(request.length);
-                bytes.set(request);
-                data = bytes.buffer;
-            }
-            const header = this.getRequestHeaders((context as HttpClientContext).httpRequestHeaders);
-            this.requests[index] = wx.request({
-                url: context.uri,
-                data,
-                header,
-                method: 'POST',
-                dataType: 'arraybuffer',
-                responseType: 'arraybuffer',
-                complete: (res: any) => {
-                    delete this.requests[index];
-                    (context as HttpClientContext).httpResponseHeaders = res.header;
-                    (context as HttpClientContext).httpStatusCode = res.statusCode;
-                    if (res.statusCode === undefined) {
-                        reject(new Error(res.errMsg));
-                    } else if (res.statusCode >= 200 && res.statusCode < 300) {
-                        resolve(new Uint8Array(res.data));
-                    } else {
-                        reject(new Error(res.statusCode + ':' + ByteStream.toString(res.data)));
-                    }
+        const result = defer<Uint8Array>();
+        const index = this.counter++;
+        let data: ArrayBuffer;
+        if (request.buffer.slice) {
+            data = request.buffer.slice(request.byteOffset, request.length);
+        } else {
+            const bytes = new Uint8Array(request.length);
+            bytes.set(request);
+            data = bytes.buffer;
+        }
+        const header = this.getRequestHeaders((context as HttpClientContext).httpRequestHeaders);
+        this.requests[index] = wx.request({
+            url: context.uri,
+            data,
+            header,
+            method: 'POST',
+            dataType: 'arraybuffer',
+            responseType: 'arraybuffer',
+            complete: (res: any) => {
+                delete this.requests[index];
+                (context as HttpClientContext).httpResponseHeaders = res.header;
+                (context as HttpClientContext).httpStatusCode = res.statusCode;
+                if (res.statusCode === undefined) {
+                    result.reject(new Error(res.errMsg));
+                } else if (res.statusCode >= 200 && res.statusCode < 300) {
+                    result.resolve(new Uint8Array(res.data));
+                } else {
+                    result.reject(new Error(res.statusCode + ':' + ByteStream.toString(res.data)));
                 }
-            });
-            if (context.timeout > 0) {
-                const timeoutId = setTimeout(() => {
-                    reject(new TimeoutError());
-                }, context.timeout);
-                result.then(() => {
-                    clearTimeout(timeoutId);
-                }, () => {
-                    clearTimeout(timeoutId);
-                });
             }
         });
-        return result;
+        if (context.timeout > 0) {
+            const timeoutId = setTimeout(() => {
+                result.reject(new TimeoutError());
+            }, context.timeout);
+            result.promise.then(() => {
+                clearTimeout(timeoutId);
+            }, () => {
+                clearTimeout(timeoutId);
+            });
+        }
+        return result.promise;
     }
     public async abort(): Promise<void> {
         for (const index in this.requests) {
