b6bbc0b8ec5ca0e5cc39d1e7255b5f042af01c14
andot <mabingyao@gmail.com>
2020-03-28 15:11:40 +0800

    Add rpc-plugin-forward

---
+++ packages/@hprose/rpc-plugin-forward/src/index.ts
@@ -0,0 +1,39 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| index.ts                                                 |
+|                                                          |
+| @hprose/rpc-plugin-forward for TypeScript.               |
+|                                                          |
+| LastModified: Mar 28, 2020                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+import { Context, NextIOHandler, NextInvokeHandler, ClientContext, Client, IOHandler, InvokeHandler } from '@hprose/rpc-core';
+
+export class Forward {
+    private readonly client: Client;
+    public constructor(uri?: string | string[]) {
+        this.client = new Client(uri)
+    }
+    public ioHandler = async (request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> => {
+        const clientContext = new ClientContext();
+        clientContext.init(this.client, null);
+        return this.client.request(request, clientContext);
+    }
+    public invokeHandler = (name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> => {
+        return this.client.invoke(name, args);
+    }
+    public use(...handlers: InvokeHandler[] | IOHandler[]): this {
+        this.client.use(...handlers);
+        return this;
+    }
+    public unuse(...handlers: InvokeHandler[] | IOHandler[]): this {
+        this.client.unuse(...handlers);
+        return this;
+    }
+}
