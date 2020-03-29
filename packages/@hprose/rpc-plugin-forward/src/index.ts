2515890ae2c2682f30a4565ab2ed55ec12047494
andot <mabingyao@gmail.com>
2020-03-29 23:02:02 +0800

    Remove http headers forward support.

--- packages/@hprose/rpc-plugin-forward/src/index.ts
+++ packages/@hprose/rpc-plugin-forward/src/index.ts
@@ -21,34 +21,16 @@ export class Forward {
     public constructor(uri?: string | string[]) {
         this.client = new Client(uri)
     }
-    private forwardHttpRequestHeaders(context: Context, clientContext: ClientContext): void {
-        if ('httpRequestHeaders' in context) {
-            clientContext['httpRequestHeaders'] = context['httpRequestHeaders'];
-        }
-    }
-    private forwardHttpResponseHeaders(context: Context, clientContext: ClientContext): void {
-        if ('httpResponseHeaders' in clientContext) {
-            context['httpResponseHeaders'] = clientContext['httpResponseHeaders'];
-        }
-        if ('httpStatusCode' in clientContext) {
-            context['httpStatusCode'] = clientContext['httpStatusCode'];
-        }
-    }
-    public ioHandler = async (request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> => {
+    public ioHandler = (request: Uint8Array, context: Context, next: NextIOHandler): Promise<Uint8Array> => {
         const clientContext = new ClientContext({ timeout: this.timeout });
         clientContext.init(this.client);
-        this.forwardHttpRequestHeaders(context, clientContext);
-        const response = await this.client.request(request, clientContext);
-        this.forwardHttpResponseHeaders(context, clientContext);
-        return response;
+        return this.client.request(request, clientContext);
     }
     public invokeHandler = async (name: string, args: any[], context: Context, next: NextInvokeHandler): Promise<any> => {
         const clientContext = new ClientContext({ timeout: this.timeout });
-        this.forwardHttpRequestHeaders(context, clientContext);
         clientContext.requestHeaders = context.requestHeaders;
         const result = await this.client.invoke(name, args, clientContext);
         context.responseHeaders = clientContext.responseHeaders;
-        this.forwardHttpResponseHeaders(context, clientContext);
         return result;
     }
     public use(...handlers: InvokeHandler[] | IOHandler[]): this {
